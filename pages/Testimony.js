e8eb254c8929f5e9521d800869dc12e61c6f09b5
soyo-kaze <07sameersaini@gmail.com>
2022-01-20 15:02:58 +0530

    testimony added

--- pages/Testimony.js
+++ pages/Testimony.js
@@ -1,14 +1,36 @@
 import { InfoProvider } from "../components/dataContext";
+import { useState, useEffect } from "react";
+import { Link } from "next/link";
 
 const Testimony = () => {
   const [state, dispatch] = InfoProvider();
+  const [isLoaded, setLoad] = useState(true);
+
+  useEffect(() => {
+    let downBtn = document.querySelector("#download");
+    downBtn.href = state.data ? state.data.image : "/assets/alt.png";
+    setLoad(false);
+  }, [state]);
+
+  const download = () => {};
   return (
-    <div className="form__container">
+    <div
+      className="form__container"
+      style={{ height: "70vh", flexDirection: "column" }}
+    >
       <img
-        src={state.data ? state.data.image : ""}
-        style={{ objectFit: "contain", height: "500px" }}
+        src={state.data ? state.data.image : "/assets/alt.png"}
+        style={{ objectFit: "contain", height: "300px" }}
       />
-      <p>{state.data ? state.data.name : ""}</p>
+      {/* <p>{state.data ? state.data.name : "someText"}</p> */}
+      <div className="testi__btns">
+        <a id="download" download="template.jpg" href="" disabled={isLoaded}>
+          <button disabled={isLoaded}>Download</button>
+        </a>
+        <Link href="/Form">
+          <button>Generate another</button>
+        </Link>
+      </div>
     </div>
   );
 };
