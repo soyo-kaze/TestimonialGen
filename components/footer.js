51cfd6467b291cb476f6e49763ee82ec2d938350
soyo-kaze <07sameersaini@gmail.com>
2022-01-21 19:49:33 +0530

    final touches 1

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <p style={{ padding: 0 }}>
+        Made with 💟 by{" "}
+        <a href="https://soyo-kaze.github.io/" target="_blank">
+          Soyokaze
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
