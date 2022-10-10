d7f6510118e1b21298342af43be21a5b7e9b1f47
Arya Kumar <arryarts@gmail.com>
2022-10-10 22:20:42 +0530

    converted_in_js

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,23 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title> TestimonialGen </title>{" "}
-        <link rel="shortcut icon" href="/assets/index.png">
-          {" "}
-        </link>{" "}
-      </Head>{" "}
+        <title> TestimonialGen </title>
+        <link rel="shortcut icon" href="/assets/index.png"></link>
+      </Head>
       <div className="hero__container">
         <div className="hero__text">
           <div className="hero__header">
-            Create your own <br> </br> <x className="testinomy"> Testinomy </x>{" "}
-          </div>{" "}
+            Create your own <br> </br> <x className="testinomy"> Testinomy </x>
+          </div>
           <div className="hero__subhead">
-            Dynamic Digital graphic testimony Generator{" "}
-          </div>{" "}
+            Dynamic Digital graphic testimony Generator
+          </div>
           <div className="hero__btn">
             <Link href="/Form">
-              <button> Create Now </button>{" "}
-            </Link>{" "}
-          </div>{" "}
-        </div>{" "}
+              <button> Create Now </button>
+            </Link>
+          </div>
+        </div>
         <div className="hero__imgContainer">
           <iframe
             className="hero__img"
@@ -32,11 +30,9 @@ const Home = () => {
             frameborder="0"
             width="100%"
             height="100%"
-          >
-            {" "}
-          </iframe>{" "}
-        </div>{" "}
-      </div>{" "}
+          ></iframe>
+        </div>
+      </div>
     </>
   );
 };
