29a9d3a6b4cfa0934f137965c5f62416e9be58d8
Arya Kumar <79188851+Arry-Minati@users.noreply.github.com>
2022-10-10 22:43:35 +0530

    Fixed issue (#8)
    * added spline
    
    * converted-jsx-to-js
    
    * converted-jsx-to-js
    
    * converted_in_js
    
    * fixed_br

--- pages/index.js
+++ pages/index.js
@@ -6,31 +6,35 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>TestimonialGen</title>
+        <title> TestimonialGen </title>
         <link rel="shortcut icon" href="/assets/index.png"></link>
       </Head>
       <div className="hero__container">
         <div className="hero__text">
           <div className="hero__header">
-            Create your own <br></br>
-            <x className="testinomy">Testinomy</x>
+            Create your own <br /> <x className="testinomy"> Testinomy </x>
           </div>
           <div className="hero__subhead">
-            Dynamic Digital graphic testimony Generator{" "}
+            Dynamic Digital graphic testimony Generator
           </div>
           <div className="hero__btn">
             <Link href="/Form">
-              <button> Create Now</button>
+              <button> Create Now </button>
             </Link>
           </div>
         </div>
         <div className="hero__imgContainer">
-         
-        <iframe className="hero__img" src='https://my.spline.design/untitled-40e6b19287e092ff31053cbd744ae16d/' frameborder='0' width='100%' height='100%'></iframe>
+          <iframe
+            className="hero__img"
+            src="https://my.spline.design/untitled-40e6b19287e092ff31053cbd744ae16d/"
+            frameBorder="0"
+            width="100%"
+            height="100%"
+          ></iframe>
         </div>
       </div>
     </>
   );
-}; 
+};
 
 export default Home;
